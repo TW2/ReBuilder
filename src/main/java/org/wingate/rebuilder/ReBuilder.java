8911603c3672c9bb0941dcb097cdb0c62d84d5c8
RedDog,csa <util2@msn.com>
2025-06-16 00:45:48 +0200

    Define as Maven project

---
+++ src/main/java/org/wingate/rebuilder/ReBuilder.java
@@ -0,0 +1,13 @@
+package org.wingate.rebuilder;
+
+import java.awt.*;
+
+public class ReBuilder {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(()->{
+            System.out.println("Re::UI::Builder is about to start...");
+
+            System.out.println("Re::UI::Builder has started!");
+        });
+    }
+}
