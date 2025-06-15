4fe485bffffc118bf0c96d140b2946212ac21c1a
RedDog,csa <util2@msn.com>
2025-06-16 01:10:34 +0200

    Main class and launcher

---
+++ src/main/java/org/wingate/rebuilder/MainFrame.java
@@ -0,0 +1,13 @@
+package org.wingate.rebuilder;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() throws HeadlessException {
+
+    }
+
+
+}
--- src/main/java/org/wingate/rebuilder/ReBuilder.java
+++ src/main/java/org/wingate/rebuilder/ReBuilder.java
@@ -1,13 +1,26 @@
 package org.wingate.rebuilder;
 
-import java.awt.*;
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.swing.JFrame;
+import java.awt.EventQueue;
 
 public class ReBuilder {
     public static void main(String[] args) {
         EventQueue.invokeLater(()->{
-            System.out.println("Re::UI::Builder is about to start...");
-
-            System.out.println("Re::UI::Builder has started!");
+            try{
+                System.out.println("Re::UI::Builder is about to start...");
+                FlatLightLaf.setup();
+                MainFrame mf = new MainFrame();
+                mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                mf.setTitle("Re::UI::Builder");
+                mf.setSize(1900, 1000);
+                mf.setLocationRelativeTo(null);
+                mf.setVisible(true);
+                System.out.println("Re::UI::Builder has started!");
+            }catch(Exception _){
+                System.exit(1);
+            }
         });
     }
 }
